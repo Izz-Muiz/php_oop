55a4c7959255d3005d559abd92606f3f27650732
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-20 21:54:20 +0800

    new : constructor

--- product.php
+++ product.php
@@ -6,33 +6,23 @@ class Product
 {
     public $judul, $penulis, $penerbit, $harga;
 
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
     public function getLabel()
     {
         return "$this->judul oleh $this->penulis";
     }
 }
 
-// $product1 = new Product();
-// $product1->judul = "Harry Potter";
-// var_dump($product1);
+$product3 = new Product("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 
-
-// $product2 =  new Product();
-// $product2->judul = "Boruto";
-// var_dump($product2);
-
-$product3 = new Product();
-$product3->judul = "Naruto";
-$product3->penulis = "Masashi Kishimoto";
-$product3->penerbit = "Shonen Jump";
-$product3->harga = 30000;
-
-echo "<hr>";
-$product4 = new Product();
-$product4->judul = "Ultraman";
-$product4->penulis = "Tsuburaya Productions";
-$product4->penerbit = "Tsuburaya Productions";
-$product4->harga = 20000;
+$product4 = new Product("Ultraman", "Tsuburaya Productions", "Tsuburaya Productions", 20000);
 
 echo "Komik : " . $product3->getLabel();
 echo "<br>";
