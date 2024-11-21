d9b222c4faeaabc678c1e23617b040acddc260a3
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-21 23:57:15 +0800

    set and get

--- product.php
+++ product.php
@@ -2,9 +2,9 @@
 
 class Product
 {
-    public $judul, $penulis, $penerbit; // all class can use this
+    private $judul, $penulis, $penerbit, $harga; // all class can use this
     protected $discount = 0; // only this and inheritance class can use this
-    private $harga; // onlu this class can use this
+    // private ; // onlu this class can use this
 
     public function __construct($judul, $penulis, $penerbit, $harga)
     {
@@ -12,6 +12,45 @@ class Product
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+    }
+
+    public function setJudul($judul)
+    {
+        $this->judul = $judul;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function getDiscount()
+    {
+        return $this->discount;
     }
 
     public function setDiscount($discount)
@@ -88,3 +127,8 @@ echo "<hr>";
 
 $product2->setDiscount(50);
 echo $product2->getHarga();
+
+echo "<hr>";
+
+$product1->setJudul("Yoi");
+echo $product1->getJudul();
