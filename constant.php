308d164f1c9d377493682be8227ec72efe5188b4
Izz-Muiz <muizzuddin0211@gmail.com>
2024-11-22 18:57:42 +0800

    learn const

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php
+
+define("NAMA", "Ali"); // cant save in class
+echo NAMA;
+
+echo "<br>";
+
+const UMUR = 32; // can save in class
+echo UMUR;
+
+
+
+// Magic Constant
+
+// echo __FILE__;
+
+// function coba()
+// {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+// class Coba
+// {
+//     public $kelas  = __CLASS__;
+// }
+
+// $obj = new Coba;
+// echo $obj->kelas; // Coba
